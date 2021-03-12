c8aa7ab85f44c994edd0a57003db9a9a0bf0af2c
louismonty <louis.mail.dk@gmail.com>
2021-03-12 09:50:18 +0100

    started on Laser in BoardElement

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElements/Laser.java
@@ -0,0 +1,36 @@
+package dk.dtu.compute.se.pisd.roborally.model.BoardElements;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.BoardElement;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+
+public class Laser extends BoardElement {
+    private whatKindOfLaser laserType;
+    private int amountOFLaser;
+    private Heading headin;
+    /**
+     * Constructor for Space.
+     *
+     * @param board       The board the space is on.
+     * @param x           the position on the horizontal axis.
+     * @param y           the position on the vertical axis.
+     * @param description
+     */
+    public Laser(Board board, int x, int y, String description, whatKindOfLaser laserType, int amountOFLaser, Heading heading) {
+        super(board, x, y, description);
+        this.laserType = laserType;
+        this.amountOFLaser = amountOFLaser;
+        this.headin = heading;
+    }
+
+    public enum whatKindOfLaser{
+        START,
+        MIDDLE,
+        END
+    }
+
+    @Override
+    public void landOnSpace() {
+        //draw damage card
+    }
+}

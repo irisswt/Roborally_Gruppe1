3ee1456a1965744520cacf7676b1fab4035742ad
louismonty <louis.mail.dk@gmail.com>
2021-03-29 15:33:04 +0200

    First commit

--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/LoadBoard.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/LoadBoard.java
@@ -102,14 +102,14 @@ public class LoadBoard {
         for (int i=0; i<board.width; i++) {
             for (int j=0; j<board.height; j++) {
                 Space space = board.getSpace(i,j);
-                /*if (!space.getWalls().isEmpty() || !space.getActions().isEmpty()) {
+                //if (!space.getWalls().isEmpty() || !space.getActions().isEmpty()) {
                     SpaceTemplate spaceTemplate = new SpaceTemplate();
                     spaceTemplate.x = space.x;
                     spaceTemplate.y = space.y;
-                    spaceTemplate.actions.addAll(space.getActions());
-                    spaceTemplate.walls.addAll(space.getWalls());
+                    //spaceTemplate.actions.addAll(space.getActions());
+                    //spaceTemplate.walls.addAll(space.getWalls());
                     template.spaces.add(spaceTemplate);
-                }*/
+
             }
         }
 

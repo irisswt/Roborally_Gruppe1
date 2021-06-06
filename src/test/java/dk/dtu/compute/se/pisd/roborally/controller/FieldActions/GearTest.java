4ca16034f371b7466f9c6154a3e085e745bd1812
niklas <niklasborner@gmail.com>
2021-06-06 12:48:03 +0200

    Moved FieldAction Tests to FieldActionsTest folder

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/FieldActions/GearTest.java
@@ -0,0 +1,45 @@
+package dk.dtu.compute.se.pisd.roborally.controller.FieldActions;
+
+import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GearTest {
+
+
+    GameController gameController;
+    @BeforeEach
+    void setUp() {
+        Board board = new Board(8, 8);
+        gameController = new GameController(board);
+        for (int i = 0; i < 6; i++) {
+            Player player = new Player(board, null, "Player " + i);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(i, i));
+            player.setHeading(Heading.values()[i % Heading.values().length]);
+        }
+        board.setCurrentPlayer(board.getPlayer(0));
+    }
+
+    @Test
+    void doAction() {
+        FieldAction fieldAction;
+        fieldAction = new Gear();
+        Space space = gameController.board.getSpace(2, 2);
+        Player player = new Player(gameController.board, "green", "P");
+        space.setPlayer(player);
+        player.setHeading(Heading.SOUTH);
+        fieldAction.doAction(gameController, space);
+        Player playerOnSpace = space.getPlayer();
+        assertEquals(Heading.WEST, playerOnSpace.getHeading());
+    }
+
+}
---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/FieldActions/PushPanelTest.java
@@ -0,0 +1,39 @@
+package dk.dtu.compute.se.pisd.roborally.controller.FieldActions;
+
+import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
+import dk.dtu.compute.se.pisd.roborally.controller.GameController;
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PushPanelTest {
+
+
+    GameController gameController;
+    @BeforeEach
+    void setUp() {
+        Board board = new Board(8, 8);
+        gameController = new GameController(board);
+        for (int i = 0; i < 6; i++) {
+            Player player = new Player(board, null, "Player " + i);
+            board.addPlayer(player);
+            player.setSpace(board.getSpace(i, i));
+            player.setHeading(Heading.values()[i % Heading.values().length]);
+        }
+        board.setCurrentPlayer(board.getPlayer(0));
+    }
+
+    @Test
+    void setHeading() {
+        PushPanel pushPanel = new PushPanel();
+        pushPanel.setHeading(Heading.SOUTH);
+        assertEquals(Heading.SOUTH, pushPanel.getHeading());
+    }
+
+}
